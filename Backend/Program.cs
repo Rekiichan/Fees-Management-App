91cc7e58956a473d596daa4f7e5b4a98b34e5c55
Rekiichan <dtnhan.ete.dut@gmail.com>
2023-02-12 00:12:27 +0700

    use top-level statement

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,46 +3,37 @@ using FeeCollectorApplication.Repository;
 using FeeCollectorApplication.Service;
 using Microsoft.EntityFrameworkCore;
 
-namespace FeeCollectorApplication
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddControllers().AddJsonOptions(
+options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//services cors
+builder.Services.AddCors(p => p.AddPolicy("AllowAllHeadersPolicy", builder =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-            builder.Services.AddControllers().AddJsonOptions(
-        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
-            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-                builder.Configuration.GetConnectionString("DefaultConnection"))
-            );
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            //services cors
-            builder.Services.AddCors(p => p.AddPolicy("AllowAllHeadersPolicy", builder =>
-            {
-                builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-            }));
-            var app = builder.Build();
-            app.UseCors("AllowAllHeadersPolicy");
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+var app = builder.Build();
+app.UseCors("AllowAllHeadersPolicy");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
