2525e152087b97f208ec63dfa1f41c54c31b8d1b
Rekiichan <dtnhan.ete.dut@gmail.com>
2023-02-21 23:44:06 +0700

    add employee role

--- Backend/Utility/SD.cs
+++ Backend/Utility/SD.cs
@@ -3,7 +3,7 @@
     public class SD
     {
         public const string Role_Admin = "admin";
-        public const string Role_Customer = "customer";
         public const string Role_Employee = "employee";
+        public const string Role_Customer = "customer";
     }
 }
