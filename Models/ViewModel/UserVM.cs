38f4d35b86efa7380448c240a73d20cad202c2bb
Dao Trong Nhan <95452303+Rekiichan@users.noreply.github.com>
2023-03-27 18:46:45 +0700

    update role list instead of string

--- Models/ViewModel/UserVM.cs
+++ Models/ViewModel/UserVM.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string Role { get; set; }
+        public List<string> Role { get; set; }
     }
 }
